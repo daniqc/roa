efa5fe001d65f5e6bdb423664e4fec279baebd95
daniqc <dani.quiroz@gmail.com>
2014-04-17 22:24:13 -0300

    add hello

--- db/schema.rb
+++ db/schema.rb
@@ -39,10 +39,10 @@ ActiveRecord::Schema.define(:version => 20131203023615) do
   end
 
   create_table "comments", :force => true do |t|
-    t.string   "comment",            :limit => 500
+    t.string   "comment"
     t.integer  "like"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.string   "ancestry"
     t.integer  "learning_object_id"
     t.integer  "person_id"
@@ -199,6 +199,16 @@ ActiveRecord::Schema.define(:version => 20131203023615) do
     t.string   "week_day"
     t.string   "start"
     t.string   "finish"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "subjects", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "units", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+courses = [
+	["MO01","Métodos de optimización","1.0","curso de optimización","1","1","1"],
+	["CPS01","Criptografía y protocolos de seguridad","1.0","Manejar el concepto y la operación de protocolos de seguridad.","3","1","1"],
+	["RC01","Redes de comunicación","1.0","Curso de redes y comunicación","1","1","1"]
+]
+
+courses.each do |code, name, version, description, category_id, career_id, department_id|
+  Course.create( code: code, name: name, version: version, description: description, category_id: category_id, career_id: career_id, department_id: department_id)
+end
+
+categories = ["Electivo", "Obligatorio", "Optativo"]
+
+categories.each do |name|
+	Category.create(name: name)
+end
+
+careers = ["Ingeniería Civil Informática", "Ingeniería Ejecución Informática"]
+
+careers.each do |name|
+	Career.create(name: name)
+end
+
+metadata = ["General","Life cycle","Educational","Rights"]
+
+metadata.each do |name|
+	MetadataSchema.create(name: name)
+end
